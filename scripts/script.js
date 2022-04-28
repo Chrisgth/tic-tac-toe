ff7c018805db7e9a241a0b02171b090381504faf
chrisgth <ksemkus00@gmail.com>
2022-04-28 08:35:24 +0300

    Created render module that displays game board

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,20 @@ const gameBoard = (() => {
         gameBoardArray,
     };
 })();
+
+// create render module that displays game board
+const render = (() => {
+    const display = () => {
+        const board = document.querySelector('.board')
+        for ( i=0; i<gameBoard.gameBoardArray.length; i++ ) {
+            let cell = document.createElement('div')
+            cell.classList.add('cell')
+            cell.textContent = gameBoard.gameBoardArray[i]
+            board.appendChild(cell);
+        }
+    }
+    return {
+        display,
+    }
+})();
+render.display();

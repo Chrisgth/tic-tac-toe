9689ed7b04e5c0d8155d25301247600c03eee707
chrisgth <ksemkus00@gmail.com>
2022-04-28 08:43:37 +0300

    Minimal styling, emptied array

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // create a module that contains the gameboard which is stored in an array
 const gameBoard = (() => {
-    let gameBoardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    let gameBoardArray = ['', '', '', '', '', '', '', '', '']
     return {
         gameBoardArray,
     };
